c74dad92c7fc8fc7dd115ef3abd212cb452afdb8
taraskl <taras.kliuchnyk@gmail.com>
2021-01-21 14:10:51 +0200

    refactoring

--- main.py
+++ main.py
@@ -10,13 +10,25 @@ def scraping(url):
     data_list = data[0].text.split('\n')
     driver.close()
 
+    new_list = list()
+    new_list.append(data_list[0])
+    lst1, lst2 =  data_list[1][:3], data_list[1][3:]
+    new_list.append(lst1)
+    new_list.extend([lst2[i:i + 10] for i in range(0, len(lst2), 10)])
+
+    for i in range(2, len(data_list)):
+        if i % 2 != 0:
+            new_list.extend(data_list[i].split(" "))
+        else:
+            new_list.append(data_list[i])
+
     # Writing in the CSV file
     filename = './cash-flow.csv'
 
     with open(filename, 'a+', newline='') as file:
         f = csv.writer(file)
-        for i in range(0, len(data_list) - 1, 2):
-            f.writerow([data_list[i], data_list[i + 1]])
+        for i in range(0, len(new_list) - 1, 5):
+            f.writerow([new_list[i], new_list[i + 1], new_list[i + 2], new_list[i + 3], new_list[i + 4]])
 
 
 if __name__ == "__main__":
